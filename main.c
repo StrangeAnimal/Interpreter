9daab1d4fdc42d02f7a48f3ce697feec1e97197e
StrangeAnimal <33088899+StrangeAnimal@users.noreply.github.com>
2017-12-05 10:46:34 +0000

    a

--- main.c
+++ main.c
@@ -1,7 +1,80 @@
 #include <stdio.h>
 
+
+char commands [200][10];
+int  variables [20];
+int currentvariable =0;
+
+
+int sum(int x,int y){return x+y;}
+
+
+int subtract(int x,int y){return x-y;}
+
+int multiply (int x,int y){return x*y;}
+
+int divide(int x,int y){return x/y;}
+
+
+int comparison(int x, int y){
+    
+    if(x ==y){
+        
+        return 1;
+        }
+    else{return 0;}
+}
+
+void declare(int x){
+    
+    variables[currentvariable]=x;
+    currentvariable++;
+    
+    
+    
+    }
+
+
+void initialise(int x){
+    variables[currentvariable]=x;
+    currentvariable++;
+    
+    }
+
+
+void print(int x){
+    
+    printf("%d",x);
+    
+    
+    }
+
+void read_words () {
+     int i = 0;
+
+ int BUFSIZE = 1000;
+ char* words[20];
+ FILE *fp = fopen("f.txt", "r");
+ if (fp == 0){
+        fprintf(stderr, "Error while opening");
+        exit(1);
+ }
+
+ words[i] = malloc(BUFSIZE);
+  while (fgets(words[i], BUFSIZE, fp)) {
+        i++;
+        words[i] = malloc(BUFSIZE);
+ } 
+
+printf("%s",words[2]);
+ 
+}
+
+
+
+
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+read_words();
+return 0;
 }
